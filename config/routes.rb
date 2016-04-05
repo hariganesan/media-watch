0f986917dd13efbe9c0e790c5d4672babd6cce1f
Hari Ganesan <hvganesan@gmail.com>
2016-04-04 20:43:54 -0400

    splash + fb login

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
 
-  root 'welcome#index'
+  root 'login#index'
+
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
